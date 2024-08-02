d218a5c4e3fa1f0788a27b2f2dd678407cb13649
Htetko510217 <htetko510217@gmai.com>
2024-08-03 03:05:31 +0700

    feat : update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,25 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '100%',
+          '@screen sm': {
+            maxWidth: '640px',
+          },
+          '@screen md': {
+            maxWidth: '768px',
+          },
+          '@screen lg': {
+            maxWidth: '1280px',
+          },
+          '@screen xl': {
+            maxWidth: '1400px',
+          },
+        }
+      })
+    }
+  ]
 }
