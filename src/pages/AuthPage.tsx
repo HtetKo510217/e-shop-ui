6fec1b79b8af607dd0192dea709b8035144e8b05
Htetko510217 <htetko510217@gmai.com>
2024-08-03 15:43:17 +0700

    feat : show password info

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -133,7 +133,7 @@ const AuthPage: React.FC = () => {
                     autoComplete="current-password"
                     required
                     className="appearance-none rounded-none relative block w-full px-3 py-2 pl-10 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-purple-500 focus:border-purple-500 focus:z-10 sm:text-sm"
-                    placeholder="Password"
+                    placeholder="Password at least 6 characters"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                   />
